6f8ea2104e57d6145af7a56817ce28e8deec6169
ewangclarkson <ewangclarks@gmail.com>
2024-02-15 12:21:36 +0100

    added and configured eureka naming service

---
+++ employee-service/src/test/java/com/vims/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vims.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
